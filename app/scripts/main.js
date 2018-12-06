266204fed1aaea4ba111cbdbec50323de71a2bc0
epsilon-skolanthasamy <suthakar.kolanthasamy@epsilon.com>
2018-12-06 21:55:51 +0530

    Commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,8 @@
- (function ($) {
-   $.fn.clickToggle = function (func1, func2) {
+ (function($) {
+   $.fn.clickToggle = function(func1, func2) {
      var funcs = [func1, func2];
      this.data('toggleclicked', 0);
-     this.click(function () {
+     this.click(function() {
        var data = $(this).data();
        var tc = data.toggleclicked;
        $.proxy(funcs[tc], this)();
@@ -13,118 +13,44 @@
  }(jQuery));
 
 
- $(function () {
+ $(function() {
 
-   // Sticky Nav functionality 
-   var stickyNav = function () { 
+   /*// Sticky Nav functionality 
+   var stickyNav = function() {
      $('.header').affix({
        offset: {
-         top: 30//Math.round($('.header').offset().top)
+         top: 30 //Math.round($('.header').offset().top)
        }
      });
    }
-   stickyNav(); 
+   stickyNav();
 
    //Before firing the position fixed bottom gutter is added
-   $('.header').on('affix.bs.affix', function () {
+   $('.header').on('affix.bs.affix', function() {
      $(this).after('<div class="placeholder-header">placeholder</div>');
      $('.placeholder-header').css('height', Math.round($(this).height()));
    });
    //Before removing the position fixed bottom gutter is removed
-   $('.header').on('affix-top.bs.affix', function () {
+   $('.header').on('affix-top.bs.affix', function() {
      $('header').find('.placeholder-header').remove();
    });
 
-   $('.usb-alert').on('closed.bs.alert', function(){
+   $('.usb-alert').on('closed.bs.alert', function() {
      stickyNav();
    });
-
-
-   $('.redemption-carousel').slick({
-     dots: false,
-     infinite: false,
-     speed: 300,
-     centerMode: false,
-     slidesToShow: 4,
-     slidesToScroll: 1,
-     prevArrow: '<button type="button" class="slick-prev"><img src="images/button-arrow-left.png"></button>',
-     nextArrow: '<button type="button" class="slick-next"><img src="images/button-arrow-right.png"></button>',
-     responsive: [{
-         breakpoint: 1024,
-         settings: {
-           slidesToShow: 3,
-           slidesToScroll: 3,
-           infinite: true,
-           dots: false,
-           arrows: false
-         }
-       },
-       {
-         breakpoint: 480,
-         settings: {
-           slidesToShow: 2,
-           slidesToScroll: 2,
-           dots: true,
-           arrows: true,
-           prevArrow: '<button type="button" class="slick-prev"><img src="images/icon-chevron-left-white.png"></button>',
-           nextArrow: '<button type="button" class="slick-next"><img src="images/icon-chevron-right-white.png"></button>'
-         }
-       }
-
-     ]
-
-   });
-
-
-   $('.recommendation-carousel').slick({
-     dots: false,
-     infinite: false,
-     speed: 300,
-     centerMode: false,
-     slidesToShow: 4,
-     slidesToScroll: 1,
-     prevArrow: '<button type="button" class="slick-prev"><img src="images/button-arrow-left.png"></button>',
-     nextArrow: '<button type="button" class="slick-next"><img src="images/button-arrow-right.png"></button>',
-     responsive: [{
-         breakpoint: 1024,
-         settings: {
-           slidesToShow: 3,
-           slidesToScroll: 3,
-           infinite: true,
-           dots: false,
-           arrows: false
-         }
-       },
-       {
-         breakpoint: 480,
-         settings: {
-           slidesToShow: 2,
-           slidesToScroll: 2,
-           dots: true,
-           arrows: true,
-           prevArrow: '<button type="button" class="slick-prev"><img src="images/icon-chevron-left-white.png"></button>',
-           nextArrow: '<button type="button" class="slick-next"><img src="images/icon-chevron-right-white.png"></button>'
-         }
-       }
-
-     ]
-
-   });
-
-   /*********** Hero Component ************/
-   $('.usb-banner-slider').slick({
+*/
+   /*********** Popular Categories Slider ************/
+   $('.categories-tiles').slick({
      dots: false,
      infinite: false,
      speed: 300,
      slidesToShow: 5,
      slidesToScroll: 1,
      arrows: false,
-     prevArrow: '<button type="button" class="slick-prev"><img src="images/icon-chevron-left.png"></button>',
-     nextArrow: '<button type="button" class="slick-next"><img src="images/icon-chevron-right.png"></button>',
      responsive: [{
          breakpoint: 1200,
          settings: {
-           slidesToShow: 3,
+           slidesToShow: 5,
            slidesToScroll: 1,
            arrows: true,
            dots: true
@@ -133,10 +59,10 @@
        {
          breakpoint: 600,
          settings: {
-           slidesToShow: 2,
+           slidesToShow: 3,
            slidesToScroll: 1,
-           arrows: true,
-           dots: true
+           centerMode: true,
+           dots: false
          }
        },
        {
@@ -144,140 +70,47 @@
          settings: {
            slidesToShow: 1,
            slidesToScroll: 1,
-           arrows: true,
-           dots: true
+           centerMode: true,
+           dots: false
          }
        }
      ]
    });
-   /*********** Hero Component ************/
 
-   /*********** Redeem Component ************/
-
-   $('.redeem-slider').slick({
-     slidesToShow: 1,
+   /*********** Popular Categories Slider ************/
+   $('.current-promotions-carousel').slick({
+     dots: false,
+     infinite: false,
+     speed: 300,
+     centerMode: false,
+     slidesToShow: 5,
      slidesToScroll: 1,
-     speed: 500,
-     arrows: false,
-     fade: false,
-     asNavFor: '.redeem-slider-nav-thumbnails',
-     autoplay: true,
-     autoplaySpeed: 3000,
-     
-     prevArrow: '<button type="button" class="slick-prev"><img src="images/icon-chevron-left-white.png"></button>',
-     nextArrow: '<button type="button" class="slick-next"><img src="images/icon-chevron-right-white.png"></button>',
+     arrows:false,
      responsive: [{
-         breakpoint: 1023,
+         breakpoint: 1024,
          settings: {
-           dots: true,
-           arrows: true,
-           asNavFor: null,
-           autoplay:false,
-           adaptiveHeight: true
+           slidesToShow: 2,
+           slidesToScroll: 1,
+           infinite: true,
+           dots: false,
+           arrows: false
          }
        },
        {
-         breakpoint: 767,
+         breakpoint: 480,
          settings: {
-           dots: true,
-           arrows: true,
-           asNavFor: null,
-           autoplay:false,
-           adaptiveHeight: true
+           slidesToShow: 1,
+           slidesToScroll: 1,
+           dots: true
          }
        }
 
      ]
-   });
 
-   $('.redeem-slider-nav-thumbnails').slick({
-     slidesToShow: 3,
-     asNavFor: '.redeem-slider',
-     dots: false,
-     speed: 500,
-     autoplay: false,
-     infinite: true,
-     focusOnSelect: true,
-     responsive: [{
-       breakpoint: 767,
-       settings: {
-         asNavFor: null
-       }
-     }]
-   });
-
-   // Remove active class from all thumbnail slides
-   $('.redeem-slider-nav-thumbnails .slick-slide').removeClass('slick-active');
-
-   // Set active class to first thumbnail slides
-   $('.redeem-slider-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
-
-   // On before slide change match active thumbnail to current slide
-   $('.redeem-slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-     var mySlideNumber = nextSlide;
-     $('.redeem-slider-nav-thumbnails .slick-slide').removeClass('slick-active');
-     $('.redeem-slider-nav-thumbnails .slick-slide[data-slick-index=' + mySlideNumber + ']').addClass('slick-active');
    });
 
 
-   $('.slick-btnPlay').clickToggle(function () {
-     $('.redeem-slider').slick('slickPause');
-     $(this).addClass('slick-pause-btn');
-   }, function () {
-     $('.redeem-slider').slick('slickPlay');
-     $(this).removeClass('slick-pause-btn');
-   });
-
-   /*********** End Redeem Component ************/
-
-   /*********** Start Product Detail Component ************/
-   $('.product-detail-carousal').slick({
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     speed: 500,
-     arrows: false,
-     fade: false,
-     asNavFor: '.product-detail-carousal-nav-thumbnails',
-     autoplay: false,
-     autoplaySpeed: 3000,
-     adaptiveHeight: true,
-     prevArrow: '<button type="button" class="slick-prev"><img src="images/icon-chevron-left.png"></button>',
-     nextArrow: '<button type="button" class="slick-next"><img src="images/icon-chevron-right.png"></button>',
-     responsive: [{
-       breakpoint: 767,
-       settings: {
-         dots: true,
-         arrows: true,
-         asNavFor: null,
-         adaptiveHeight: true,
-         infinite: false
-       }
-     }]
-   });
-
-   $('.product-detail-carousal-nav-thumbnails').slick({
-     slidesToShow: 3,
-     slidesToScroll: 1,
-     asNavFor: '.product-detail-carousal',
-     dots: false,
-     focusOnSelect: true,
-     infinite: false,
-     arrows: false
-   });
-
-   // Remove active class from all thumbnail slides
-   $('.product-detail-carousal-nav-thumbnails .slick-slide').removeClass('slick-active');
-
-   // Set active class to first thumbnail slides
-   $('.product-detail-carousal-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
-
-   // On before slide change match active thumbnail to current slide
-   $('.product-detail-carousal').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-     var mySlideNumber = nextSlide;
-     $('.product-detail-carousal-nav-thumbnails .slick-slide').removeClass('slick-active');
-     $('.product-detail-carousal-nav-thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
-   });
-   /*********** End Product Detail Component ************/
+   /*********** Redeem Component ************/
 
    /*********** Sub Catalog Component ************/
    $('#range').ionRangeSlider({
@@ -303,10 +136,10 @@
    $(window).resize(accordion4mobile);
 
    //Range slider functionality 
-   $('#range').on('change', function (e) {
+   $('#range').on('change', function(e) {
      var sliderVal = $(this).val();
      var redeemVal;
-     $('.product-list').each(function () {
+     $('.product-list').each(function() {
        redeemVal = $(this).data('redeem-point');
        if (redeemVal <= sliderVal) {
          $(this).closest('.product-wrapper').removeClass('show hide').addClass('show');
@@ -318,10 +151,10 @@
 
    /*********** End Sub Catalog Component ************/
    /******* Rewards Component **********/
-   $(document).on('click', '.order-data .order-header',function () {
-     if($(this).hasClass('collapsed')){
-       $(this).find('.icon-toggle').attr('class','glyphicon glyphicon-triangle-right icon-toggle');
-     } else{
+   $(document).on('click', '.order-data .order-header', function() {
+     if ($(this).hasClass('collapsed')) {
+       $(this).find('.icon-toggle').attr('class', 'glyphicon glyphicon-triangle-right icon-toggle');
+     } else {
        $(this).find('.icon-toggle').attr('class', 'glyphicon glyphicon-triangle-bottom icon-toggle');
      }
    });
